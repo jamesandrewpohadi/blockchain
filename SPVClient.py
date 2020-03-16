bf5009e5e0e247580bac59930945b8dc9395c9b0
Kundan Dalmia <kundandalmia@gmail.com>
2020-03-17 06:13:21 +0800

    updated SPVClient
    updated SPVClient

--- SPVClient.py
+++ SPVClient.py
@@ -4,6 +4,8 @@ import ecdsa
 from time import time
 from utils import Transaction
 from utils import MerkleTree
+import json
+
 class SPVClient:
     def __init__(self):
         self.private_key = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
@@ -23,7 +25,23 @@ class SPVClient:
         proof_of_path = MerkleTree.get_proof(transaction_hash_value) #Need to create a merkle tree of transactions by the miner in the miner class so as to prove and get the path and root
         get_root = MerkleTree.get_root()                             #Should be miner here instead of Merkle Tree
 
+        if proof_of_path != None:
+            for i in proof_of_path:
+                if i[0] == 0:
+                    string = str(i[1]) + str(transaction_hash_value)
+                    transaction_hash_value = hashlib.sha256(string.encode()).hexdigest()
 
+                else:
+                    string = str(transaction_hash_value) + str(i[1])
+                    transaction_hash_value = hashlib.sha256(string.encode()).hexdigest()
+        else:
+            return False
+        
+        for i in header:
+            json_data = json.loads(i)
+            if str(json_data.get('root_hash'))== str(transaction_hash_value):
+                return True
+        return False
 
     def send_transaction(self, receiver, amount, comment):
         transaction = Transaction.new(self.public_key.to_string(), receiver, amount, comment)
